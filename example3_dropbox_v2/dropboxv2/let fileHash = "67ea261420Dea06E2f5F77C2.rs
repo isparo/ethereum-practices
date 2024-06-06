5f9cf1febc785298d578e1d7dd28378659fbe615
isparo <josueparra2892@gmail.com>
2024-06-06 12:40:44 -0600

    added desc

---
+++ example3_dropbox_v2/dropboxv2/let fileHash = "67ea261420Dea06E2f5F77C2.rs
@@ -0,0 +1,11 @@
+let fileHash = "67ea261420Dea06E2f5F77C204128E20fc40f02aafe14"; // Reemplaza con tu valor real
+let fileSize = 1024;
+let fileType = "image/png";
+let fileName = "example.png";
+let fileDescription = "An example file";
+let v = 27;
+let r = "0x0116c30374dcf55be1a626d2e37b6f88da121c2c62f991bd6d443825b91f4671"; // Reemplaza con tu valor real
+let s = "0x6478acd8f19f17fba8ee851c590d3bc6d60924d99aadd7599be7fbc00063f930"; // Reemplaza con tu valor real
+
+
+instance.uploadFile(fileHash,fileSize,fileType,fileName,fileDescription,v,r,s)
